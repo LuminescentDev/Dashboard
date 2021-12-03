c263c2a6292716f6060fdf0ee531a5369b27fef5
RiZeFlame <oliver.flynn025@gmail.com>
2021-12-02 16:25:22 -0800

    a

--- database/models/topggVoteGet.js
+++ database/models/topggVoteGet.js
@@ -6,20 +6,18 @@ function sleep(ms) {
 
 module.exports = async (client, info) => {
 
-	console.log(info)
-
 	//initiate the embed
 	let voteEmbed = new MessageEmbed()
     .setTitle("Vote Received")
     .setURL(`https://top.gg/bot/${client.user.id}/vote`);
 	try {
 		//select the user from the database
-		const user = await client.getUser(info.id);
+		const user = await client.getUser(info.user);
 			//if user does not exist add them
 			if(!user) {
 				require("./user")(client, info.user);
 				sleep(100);
-				client.con.query(`UPDATE Users Set voteTotal = 1, lastVote = ${Date.now()} where userID = "${info.user}"`);
+				client.con.query(`UPDATE Users Set voteTotal = 1, lastVote = ${Date.now()} where userID = ${info.user}`);
 				//set embed description and send it to the vote channel
 				voteEmbed.setDescription(`Thank you for voting ${client.users.cache.get(info.user).username}\n You now have 1 Vote!\n You can vote again in <t:${Math.floor(Date.now()/1000 + 43200)}:R>`);
 				client.channels.cache.get(client.config.votechannel).send({ embeds: [voteEmbed]});
